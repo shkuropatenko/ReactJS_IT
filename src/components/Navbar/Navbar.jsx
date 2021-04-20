9d86d9b5a2d0a8b9f885d4d8200bf1cbb20b59af
Dmitriy <shkuropatenko.d@gmail.com>
2021-04-20 15:54:05 +0300

    urok 20 NavLink dobovlyaem active ssulki i uberaem perezagruzku stranic

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile' activeClassName={classes.activeLink}>Profile</NavLink>
             </div>
-            <div className={`${classes.item} ${classes.active}`}>
-                <a href='/dialog'>Messages</a>
-            </div>
-            <div className={classes.item}>
-                <a href='#'>News</a>
+            <div className={`${classes.item}`}>
+                <NavLink to='/dialogs' activeClassName={classes.activeLink}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='#'>Music</a>
+                <NavLink to='/news' activeClassName={classes.activeLink}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a href='#'>Settings</a>
+                <NavLink to='/music' activeClassName={classes.activeLink}>Music</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to='/settings' activeClassName={classes.activeLink}>Settings</NavLink>
             </div>
         </nav>
     );
